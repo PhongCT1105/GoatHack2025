9083044570e6c44a35842a6fb6c033645da1e5b7
wolflieu201105 <201105phamgiahuy@gmail.com>
2025-01-17 22:45:39 -0500

    basic about us format

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,15 +1,37 @@
 import React from 'react';
+import "./About.css";
+import phong from './Github_Icons/phong.jpg'
+import doanh from './Github_Icons/doanh.jpg'
+import hien from './Github_Icons/hien.gif'
+import huy from './Github_Icons/huy.jpg'
 
 const About = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold mb-4">About Us</h1>
-        <p className="text-lg text-gray-600">
-          This is the About page. Learn more about our mission and the purpose of this application.
-        </p>
+    <body>
+      <div id='Why'>
+        <span className='large_text'>Why do we do this</span>
+        <span className='medium_text'>Cause we like to move it move it Cause we like to move it move it Cause we like to move it move it </span>
       </div>
-    </div>
+      <div className="flex items-center justify-center bg-gray-100">
+        <div className='Person'>
+          <img className='imageIcon' src={doanh} alt="Le Dang Doanh Phung"/>
+          <div>Le Dang Doanh Phung</div>
+        </div>
+        <div className='Person'>
+          <img className='imageIcon' src={hien} alt="eric Hien"/>
+          <div>eric Hien</div>
+        </div>
+        <div className='Person'>
+          <img className='imageIcon' src={phong} alt="Cao Thanh Phong"/>
+          <div>Cao Thanh Phong</div>
+        </div>
+        <div className='Person'>
+          <img className='imageIcon' src={huy} alt="Pham Gia Huy"/>
+          <div>Pham Gia Huy</div>
+        </div>
+      </div>
+      <script src="About.js"></script>
+    </body>
   );
 };
 
