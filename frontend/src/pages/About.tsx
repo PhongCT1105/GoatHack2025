e6c520990ef10b7f10a8051339aad0e1e4c2f880
hienhg <giahienhoang99@gmail.com>
2025-01-18 20:06:12 -0500

    uia cat component working on about page

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -3,29 +3,33 @@ import phong from './Github_Icons/phong.jpg'
 import doanh from './Github_Icons/doanh.jpg'
 import hien from './Github_Icons/hien.gif'
 import huy from './Github_Icons/huy.jpg'
+import CatComponent from '../components/CatComponent';
 
 const About = () => {
   return (
-    <body>
-      <div id='Why'>
-        <span className='large_text'>Why do we do this</span>
-        <span className='medium_text'>Cause we like to move it move it Cause we like to move it move it Cause we like to move it move it </span>
+    <body className="flex flex-col items-center justify-center h-screen">
+      <div id='Why' className="flex flex-col items-center justify-center h-screen">
+        <span className='large_text'>Why we do this</span>
+        <span className='medium_text'>Because we uiiiaiuiiiai </span>
+        <div className="flex justify-center">
+        <CatComponent />
+      </div>
       </div>
       <div className="flex items-center justify-center">
         <div className='Person'>
-          <img className='imageIcon' src={doanh} alt="Le Dang Doanh Phung"/>
+          <img className='imageIcon' src={doanh} alt="Le Dang Doanh Phung" />
           <div>Le Dang Doanh Phung</div>
         </div>
         <div className='Person'>
-          <img className='imageIcon' src={hien} alt="eric Hien"/>
+          <img className='imageIcon' src={hien} alt="Hien Hoang" />
           <div>eric Hien</div>
         </div>
         <div className='Person'>
-          <img className='imageIcon' src={phong} alt="Cao Thanh Phong"/>
+          <img className='imageIcon' src={phong} alt="Cao Thanh Phong" />
           <div>Cao Thanh Phong</div>
         </div>
         <div className='Person'>
-          <img className='imageIcon' src={huy} alt="Pham Gia Huy"/>
+          <img className='imageIcon' src={huy} alt="Pham Gia Huy" />
           <div>Pham Gia Huy</div>
         </div>
       </div>
